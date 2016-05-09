282de20058224374a558e772ec7519ffa701b2d1
Teodor92 <teodor.ivanov92@gmail.com>
2016-05-10 00:28:25 +0300

    Activated automatic migrations

--- source/Data/Te4Fest.Data/Migrations/DefaultMigrationConfiguration.cs
+++ source/Data/Te4Fest.Data/Migrations/DefaultMigrationConfiguration.cs
@@ -1,66 +1,19 @@
-namespace ComponentCms.Data.Migrations
+namespace Te4Fest.Data.Migrations
 {
-    using System;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
-    using ComponentCms.Common.Constants;
-    using ComponentCms.Data.Models;
+    using Te4Fest.Data;
 
-    using Microsoft.AspNet.Identity;
-    using Microsoft.AspNet.Identity.EntityFramework;
-
-    public class DefaultMigrationConfiguration : DbMigrationsConfiguration<ComponentCmsDbContext>
+    public class DefaultMigrationConfiguration : DbMigrationsConfiguration<Te4FestDbContext>
     {
-        private readonly string[] defaultRoles =
-        {
-            RoleConstants.Administrator,
-            RoleConstants.SpinTrusted,
-            RoleConstants.RegFishTrusted,
-            RoleConstants.HuGtoTrusted,
-            RoleConstants.FishRegHuTrusted,
-            RoleConstants.SpinSecondTrusted
-        };
-
         public DefaultMigrationConfiguration()
         {
             this.AutomaticMigrationsEnabled = true;
             this.AutomaticMigrationDataLossAllowed = true;
         }
 
-        protected override void Seed(ComponentCmsDbContext context)
+        protected override void Seed(Te4FestDbContext context)
         {
-            foreach (var role in this.defaultRoles)
-            {
-                if (!context.Roles.Any(x => x.Name == role))
-                {
-                    context.Roles.Add(new IdentityRole(role));
-                    context.SaveChanges();
-                }
-            }
-
-            if (!context.Users.Any())
-            {
-                var userManger = new UserManager<User>(new UserStore<User>(context));
-                var result = userManger.CreateAsync(
-                    new User { Email = "info@bggrinders.com", UserName = "info@bggrinders.com", CreatedOn = DateTime.Now },
-                    "BgGrindersPassWordQ123456").Result;
-
-                var user = context.Users.FirstOrDefault(x => x.Email == "info@bggrinders.com");
-                if (user != null)
-                {
-                    var userId = user.Id;
-                    var role = context.Roles.FirstOrDefault(x => x.Name == RoleConstants.Administrator);
-
-                    role.Users.Add(new IdentityUserRole
-                    {
-                        UserId = userId,
-                        RoleId = role.Id
-                    });
-                }
-
-                context.SaveChanges();
-            }
         }
     }
 }
