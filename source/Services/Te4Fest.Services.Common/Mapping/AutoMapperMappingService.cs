b30338866e1997898e219cfc69a7957ffad30a64
Teodor92 <teodor.ivanov92@gmail.com>
2016-05-09 23:28:46 +0300

    Added a mapping service #1

---
+++ source/Services/Te4Fest.Services.Common/Mapping/AutoMapperMappingService.cs
@@ -0,0 +1,24 @@
+﻿namespace Te4Fest.Services.Common.Mapping
+{
+    using System.Linq;
+
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+
+    using Te4Fest.Services.Common.Mapping.Contracts;
+
+    public class AutoMapperMappingService : IMappingService
+    {
+        public TDestination Map<TDestination>(object source) => Mapper.Map<TDestination>(source);
+
+        public TDestination Map<TSource, TDestination>(TSource source, TDestination destination)
+        {
+            return Mapper.Map(source, destination);
+        }
+
+        public IQueryable<TDestination> MapCollection<TDestination>(IQueryable source, object parameters = null)
+        {
+            return source.ProjectTo<TDestination>(parameters);
+        }
+    }
+}
---
+++ source/Services/Te4Fest.Services.Common/Mapping/Contracts/IMappingService.cs
@@ -0,0 +1,15 @@
+﻿namespace Te4Fest.Services.Common.Mapping.Contracts
+{
+    using System.Linq;
+
+    using Te4Fest.Services.Common;
+
+    public interface IMappingService : IService
+    {
+        TDestination Map<TDestination>(object source);
+
+        TDestination Map<TSource, TDestination>(TSource source, TDestination destination);
+
+        IQueryable<TDestination> MapCollection<TDestination>(IQueryable source, object parameters = null);
+    }
+}
