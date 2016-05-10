e2e994f81ae472cda6449eaf6d0c7032988072cc
Teodor92 <teodor.ivanov92@gmail.com>
2016-05-10 23:52:09 +0300

    Added base grunt config file #8

---
+++ source/Gruntfile.js
@@ -0,0 +1,43 @@
+/// <binding AfterBuild='concurrent:lintingAndTesting' />
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        eslint: {
+            te4Fest: {
+                src: ["Web/Scripts/*.js"]
+            },
+        },
+        jasmine: {
+            osmmap: {
+                src: [''],
+                options: {
+                    specs: '',
+                    vendor: [''],
+                    outfile: '_SpecRunnerTe4Fest.html',
+                    template: require('grunt-template-jasmine-istanbul'),
+                    templateOptions: {
+                        coverage: 'bin/coverage/Te4Fest/coverage.json',
+                        report: 'bin/coverage/Te4Fest',
+                        //thresholds: {
+                        //    lines: 80,
+                        //    statements: 80,
+                        //    branches: 80,
+                        //    functions: 80
+                        //}
+                    }
+                }
+            }
+        },
+        concurrent: {
+            lintingAndTesting: [
+                'jasmine:te4Fest',
+                'eslint:te4Fest']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks("gruntify-eslint");
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-notify');
+    grunt.registerTask("default", ["concurrent:lintingAndTesting"]);
+};
