bca7a537459514d137a9724b0662ce3b51145dc7
KristianMariyanov <kristian.mariyanov@gmail.com>
2016-05-17 22:50:46 +0300

    Separate bundles to 2 methods, one for styles and one for scripts

--- source/Web/Te4Fest.Web/Config/BundleConfig.cs
+++ source/Web/Te4Fest.Web/Config/BundleConfig.cs
@@ -4,27 +4,33 @@
 
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            RegisterScriptBundles(bundles);
+            RegisterStyleBundles(bundles);
+        }
+
+        private static void RegisterStyleBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/bootstrap/bootstrap.css",
+                "~/Content/custom/site.css"));
+        }
+
+        private static void RegisterScriptBundles(BundleCollection bundles)
+        {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/vendor/jquery/jquery-{version}.js"));
+                "~/Scripts/vendor/jquery/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/vendor/jquery/jquery.validate*"));
+                "~/Scripts/vendor/jquery/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/vendor/modernizr/modernizr-*"));
+                "~/Scripts/vendor/modernizr/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/vendor/bootstrap/bootstrap.js",
-                      "~/Scripts/vendor/respond/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap/bootstrap.css",
-                      "~/Content/custom/site.css"));
+                "~/Scripts/vendor/bootstrap/bootstrap.js",
+                "~/Scripts/vendor/respond/respond.js"));
         }
     }
 }
