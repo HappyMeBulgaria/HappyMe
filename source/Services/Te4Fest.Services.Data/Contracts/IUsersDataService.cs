d71d6a161f3a5a06d444ed1d93d22d412d840a9e
KristianMariyanov <kristian.mariyanov@gmail.com>
2016-05-17 22:18:34 +0300

    Added UserDataService with method for get user by username

---
+++ source/Services/Te4Fest.Services.Data/Contracts/IUsersDataService.cs
@@ -0,0 +1,10 @@
+﻿namespace Te4Fest.Services.Data.Contracts
+{
+    using Te4Fest.Data.Models;
+    using Te4Fest.Services.Common;
+
+    public interface IUsersDataService : IService
+    {
+        User GetUserByUsername(string username);
+    }
+}
---
+++ source/Services/Te4Fest.Services.Data/UsersDataService.cs
@@ -0,0 +1,23 @@
+﻿namespace Te4Fest.Services.Data
+{
+    using System.Linq;
+
+    using Te4Fest.Data.Contracts.Repositories;
+    using Te4Fest.Data.Models;
+    using Te4Fest.Services.Data.Contracts;
+
+    public class UsersDataService : IUsersDataService
+    {
+        private readonly IDeletableEntityRepository<User> usersData;
+
+        public UsersDataService(IDeletableEntityRepository<User> usersData)
+        {
+            this.usersData = usersData;
+        }
+
+        public User GetUserByUsername(string username)
+        {
+            return this.usersData.All().FirstOrDefault(u => u.UserName == username);
+        }
+    }
+}
