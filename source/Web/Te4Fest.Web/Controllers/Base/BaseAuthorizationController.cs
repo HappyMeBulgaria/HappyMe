e27a120329d16c66fe68370daf7427e384754329
KristianMariyanov <kristian.mariyanov@gmail.com>
2016-05-17 22:19:46 +0300

    Added BaseController and BaseAuthorizationController with cached current user profile

---
+++ source/Web/Te4Fest.Web/Controllers/Base/BaseAuthorizationController.cs
@@ -0,0 +1,27 @@
+﻿namespace Te4Fest.Web.Controllers.Base
+{
+    using Te4Fest.Data.Models;
+    using Te4Fest.Services.Data.Contracts;
+
+    public class BaseAuthorizationController : BaseController
+    {
+        public BaseAuthorizationController(IUsersDataService userData)
+        {
+            this.UsersData = userData;
+            this.SetCurrentUser();
+        }
+
+        protected IUsersDataService UsersData { get; set; }
+
+        protected User UserProfile { get; set; }
+
+        private void SetCurrentUser()
+        {
+            var username = this.User.Identity.Name;
+            if (username != null)
+            {
+                this.UserProfile = this.UsersData.GetUserByUsername(username);
+            }
+        }
+    }
+}
---
+++ source/Web/Te4Fest.Web/Controllers/Base/BaseController.cs
@@ -0,0 +1,8 @@
+﻿namespace Te4Fest.Web.Controllers.Base
+{
+    using System.Web.Mvc;
+
+    public class BaseController : Controller
+    {
+    }
+}
