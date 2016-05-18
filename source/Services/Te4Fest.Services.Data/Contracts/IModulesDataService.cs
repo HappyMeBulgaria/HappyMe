ca7af4a172a3dc3b13dafdef7d917b7d0459a1cd
KristianMariyanov <kristian.mariyanov@gmail.com>
2016-05-18 20:04:45 +0300

    Added Module data service with 2 simple methods

---
+++ source/Services/Te4Fest.Services.Data/Contracts/IModulesDataService.cs
@@ -0,0 +1,14 @@
+﻿namespace Te4Fest.Services.Data.Contracts
+{
+    using System.Linq;
+
+    using Te4Fest.Data.Models;
+    using Te4Fest.Services.Common;
+
+    public interface IModulesDataService : IService
+    {
+        IQueryable<Module> GetAllModules();
+
+        IQueryable<Module> GetAllActiveModules();
+    }
+}
---
+++ source/Services/Te4Fest.Services.Data/ModulesDataService.cs
@@ -0,0 +1,22 @@
+﻿namespace Te4Fest.Services.Data
+{
+    using System.Linq;
+
+    using Te4Fest.Data.Contracts.Repositories;
+    using Te4Fest.Data.Models;
+    using Te4Fest.Services.Data.Contracts;
+
+    public class ModulesDataService : IModulesDataService
+    {
+        private readonly IDeletableEntityRepository<Module> modulesRepository;
+
+        public ModulesDataService(IDeletableEntityRepository<Module> modulesRepository)
+        {
+            this.modulesRepository = modulesRepository;
+        }
+
+        public IQueryable<Module> GetAllModules() => this.modulesRepository.All();
+
+        public IQueryable<Module> GetAllActiveModules() => this.modulesRepository.All().Where(m => m.IsActive);
+    }
+}
