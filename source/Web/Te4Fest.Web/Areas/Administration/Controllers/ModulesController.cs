17d27afc3513971916142ab4f32de7c906c26808
KristianMariyanov <kristian.mariyanov@gmail.com>
2016-05-18 22:20:30 +0300

    Added ModulesAdministrationService with action for getting ordered modules

--- source/Web/Te4Fest.Web/Areas/Administration/Controllers/ModulesController.cs
+++ source/Web/Te4Fest.Web/Areas/Administration/Controllers/ModulesController.cs
@@ -4,6 +4,7 @@
     using System.Web.Mvc;
 
     using Te4Fest.Data.Models;
+    using Te4Fest.Services.Administration;
     using Te4Fest.Services.Administration.Base;
     using Te4Fest.Services.Common.Mapping.Contracts;
     using Te4Fest.Services.Data.Contracts;
@@ -12,11 +13,11 @@
 
     public class ModulesController : BaseController
     {
-        private readonly AdministrationService<Module> modulesAdministrationService;
+        private readonly ModulesAdministrationService modulesAdministrationService;
         private readonly IMappingService mappingService;
 
         public ModulesController(
-            AdministrationService<Module> modulesAdministrationService, 
+            ModulesAdministrationService modulesAdministrationService, 
             IMappingService mappingService)
         {
             this.mappingService = mappingService;
@@ -25,7 +26,8 @@
 
         public ActionResult Index()
         {
-            var model = this.mappingService.MapCollection<ModuleGridViewModel>(this.modulesAdministrationService.Read().OrderBy(m => m.Id));
+            var model = this.mappingService.MapCollection<ModuleGridViewModel>(
+                this.modulesAdministrationService.GetAllOrderedModules());
             return this.View(model);
         }
     }
