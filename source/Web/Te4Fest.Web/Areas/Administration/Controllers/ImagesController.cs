3cdc5fbe1aa17381fbf2f8bc14a16289c08bcf0f
Teodor92 <teodor.ivanov92@gmail.com>
2016-05-18 21:28:15 +0300

    DataTables prototype admin

--- source/Web/Te4Fest.Web/Areas/Administration/Controllers/ImagesController.cs
+++ source/Web/Te4Fest.Web/Areas/Administration/Controllers/ImagesController.cs
@@ -1,24 +1,42 @@
 ﻿namespace Te4Fest.Web.Areas.Administration.Controllers
 {
+    using System.Linq;
     using System.Web.Mvc;
+
+    using Mvc.JQuery.DataTables;
 
     using Te4Fest.Data.Models;
     using Te4Fest.Services.Administration.Contracts;
+    using Te4Fest.Services.Common.Mapping.Contracts;
+    using Te4Fest.Web.Areas.Administration.ViewModels.Images;
 
     using Web.Controllers.Base;
 
     public class ImagesController : BaseController
     {
-        private readonly IAdministrationService<Image> imageAdministrationService; 
+        private readonly IAdministrationService<Image> imageAdministrationService;
+        private readonly IMappingService mapingService;
 
-        public ImagesController(IAdministrationService<Image> imageAdministrationService)
+        public ImagesController(
+            IAdministrationService<Image> imageAdministrationService,
+            IMappingService mapingService)
         {
             this.imageAdministrationService = imageAdministrationService;
+            this.mapingService = mapingService;
         }
 
         public ActionResult Index()
         {
-            return this.View();
+            var getDataUrl = this.Url.Action(nameof(ImagesController.GetImages));
+            var vm = DataTablesHelper.DataTableVm<ImageGridViewModel>("imagesGridTable", getDataUrl);
+
+            return this.View(vm);
+        }
+
+        public DataTablesResult<ImageGridViewModel> GetImages(DataTablesParam dataTableParam)
+        {
+            var imagesViewModels = this.mapingService.MapCollection<ImageGridViewModel>(this.imageAdministrationService.Read());
+            return DataTablesResult.Create(imagesViewModels, dataTableParam);
         }
     }
 }
