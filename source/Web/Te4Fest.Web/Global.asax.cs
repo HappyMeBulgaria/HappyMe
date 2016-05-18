da213e6fc7c4bbfa06ad823860e388cbb152d0d4
KristianMariyanov <kristian.mariyanov@gmail.com>
2016-05-18 20:03:01 +0300

    Added configuration of AutoMapper in Global.asax.cs

--- source/Web/Te4Fest.Web/Global.asax.cs
+++ source/Web/Te4Fest.Web/Global.asax.cs
@@ -1,11 +1,13 @@
 ﻿namespace Te4Fest.Web
 {
     using System.Data.Entity;
+    using System.Reflection;
     using System.Web;
     using System.Web.Mvc;
     using System.Web.Optimization;
     using System.Web.Routing;
 
+    using Te4Fest.Common.Mapping;
     using Te4Fest.Data;
     using Te4Fest.Data.Migrations;
 
@@ -19,6 +21,8 @@
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            AutoMapperConfig.RegisterMappings(Assembly.GetExecutingAssembly());
         }
     }
 }
