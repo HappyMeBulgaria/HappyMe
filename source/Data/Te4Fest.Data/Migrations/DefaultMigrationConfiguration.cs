26ae08bb2a039308ac7e77b27ff57342d534754d
KristianMariyanov <kristian.mariyanov@gmail.com>
2016-05-18 20:19:22 +0300

    Added sample modules in seed method for testing

--- source/Data/Te4Fest.Data/Migrations/DefaultMigrationConfiguration.cs
+++ source/Data/Te4Fest.Data/Migrations/DefaultMigrationConfiguration.cs
@@ -1,8 +1,10 @@
 namespace Te4Fest.Data.Migrations
 {
     using System.Data.Entity.Migrations;
+    using System.Linq;
 
     using Te4Fest.Data;
+    using Te4Fest.Data.Models;
 
     public class DefaultMigrationConfiguration : DbMigrationsConfiguration<Te4FestDbContext>
     {
@@ -14,6 +16,20 @@ namespace Te4Fest.Data.Migrations
 
         protected override void Seed(Te4FestDbContext context)
         {
+            if (!context.Modules.Any())
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    var module = new Module
+                    {
+                        Name = $"Test Name{i}",
+                        Description = $"Some description{i}",
+                        IsActive = i % 2 == 0
+                    };
+                    context.Modules.AddOrUpdate(module);
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
