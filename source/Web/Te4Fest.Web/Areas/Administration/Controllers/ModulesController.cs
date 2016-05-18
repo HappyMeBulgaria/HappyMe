94d8b4e6d683056528f0f5645372b7db88650bf7
KristianMariyanov <kristian.mariyanov@gmail.com>
2016-05-18 20:05:44 +0300

    Added Models Controller, view model and view for testing Grid.MVC5

---
+++ source/Web/Te4Fest.Web/Areas/Administration/Controllers/ModulesController.cs
@@ -0,0 +1,27 @@
+﻿namespace Te4Fest.Web.Areas.Administration.Controllers
+{
+    using System.Web.Mvc;
+
+    using Te4Fest.Services.Common.Mapping.Contracts;
+    using Te4Fest.Services.Data.Contracts;
+    using Te4Fest.Web.Areas.Administration.ViewModels.Modules;
+    using Te4Fest.Web.Controllers.Base;
+
+    public class ModulesController : BaseController
+    {
+        private readonly IModulesDataService modulesData;
+        private readonly IMappingService mappingService;
+
+        public ModulesController(IModulesDataService modulesData, IMappingService mappingService)
+        {
+            this.modulesData = modulesData;
+            this.mappingService = mappingService;
+        }
+
+        public ActionResult Index()
+        {
+            var model = this.mappingService.MapCollection<ModuleGridViewModel>(this.modulesData.GetAllModules());
+            return this.View(model);
+        }
+    }
+}
---
+++ source/Web/Te4Fest.Web/Areas/Administration/ViewModels/Modules/ModuleGridViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Te4Fest.Web.Areas.Administration.ViewModels.Modules
+{
+    using Te4Fest.Common.Mapping;
+    using Te4Fest.Data.Models;
+
+    public class ModuleGridViewModel : IMapFrom<Module>
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public string AuthorId { get; set; }
+    }
+}
