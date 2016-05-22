ee18adbfe018aa25c33a7295a611c01be22d4947
KristianMariyanov <kristian.mariyanov@gmail.com>
2016-05-22 14:36:12 +0300

    Added methods for create and update of modules

--- source/Web/Te4Fest.Web/Areas/Administration/Controllers/ModulesController.cs
+++ source/Web/Te4Fest.Web/Areas/Administration/Controllers/ModulesController.cs
@@ -3,6 +3,7 @@
     using System.Linq;
     using System.Web.Mvc;
 
+    using Te4Fest.Common.Constants;
     using Te4Fest.Data.Models;
     using Te4Fest.Services.Administration;
     using Te4Fest.Services.Common.Mapping.Contracts;
@@ -19,5 +20,39 @@
         }
 
         public ActionResult Index() => this.View(this.GetData().OrderBy(x => x.Id));
+
+        [HttpGet]
+        public ActionResult Create() => this.View();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ModuleGridViewModel model)
+        {
+            var entity = this.BaseCreate(model);
+            if (entity != null)
+            {
+                this.TempData[GlobalConstants.SuccessMessage] = "Успешно създадохте модул";
+                this.RedirectToAction(nameof(this.Index));
+            }
+
+            return this.View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Update() => this.View();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Update(ModuleGridViewModel model)
+        {
+            var entity = this.BaseUpdate(model);
+            if (entity != null)
+            {
+                this.TempData[GlobalConstants.SuccessMessage] = "Успешно редактирахте модул";
+                this.RedirectToAction(nameof(this.Index));
+            }
+
+            return this.View(model);
+        }
     }
 }
--- source/Web/Te4Fest.Web/Areas/Administration/ViewModels/Modules/ModuleGridViewModel.cs
+++ source/Web/Te4Fest.Web/Areas/Administration/ViewModels/Modules/ModuleGridViewModel.cs
@@ -1,11 +1,14 @@
 ﻿namespace Te4Fest.Web.Areas.Administration.ViewModels.Modules
 {
+    using System.Web.Mvc;
+
     using Te4Fest.Common.Mapping;
     using Te4Fest.Data.Contracts;
     using Te4Fest.Data.Models;
 
     public class ModuleGridViewModel : IMapFrom<Module>, IMapTo<Module>, IIdentifiable<int>
     {
+        [HiddenInput(DisplayValue = false)]
         public int Id { get; set; }
 
         public string Name { get; set; }
@@ -14,6 +17,7 @@
 
         public bool IsActive { get; set; }
 
+        [HiddenInput(DisplayValue = false)]
         public string AuthorId { get; set; }
     }
 }
