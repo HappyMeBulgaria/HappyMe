d4d29dd8763403d7cbb82a550e45a23463293943
KristianMariyanov <kristian.mariyanov@gmail.com>
2016-05-22 18:42:06 +0300

    Added js helper for editors

---
+++ source/Web/Te4Fest.Web/Scripts/custom/administration/common/editorHelper.js
@@ -0,0 +1,31 @@
+﻿'use strict';
+
+var editor = editor || {};
+
+editor = (function () {
+    function loadCKEditor(idSelector, height) {
+        CKEDITOR.replace(idSelector);
+        CKEDITOR.config.height = height || 300;
+    }
+    function unloadCKEditor(idSelector) {
+        CKEDITOR.remove(idSelector);
+    }
+    function checkForUnsavedText(text) {
+        document.body.onbeforeunload = function() {
+            for (var editorName in CKEDITOR.instances) {
+                if (CKEDITOR.instances.hasOwnProperty(editorName)) {
+                    if (CKEDITOR.instances[editorName].checkDirty()) {
+                        return text;
+                    }
+                }
+            }
+        };
+    }
+
+
+    return {
+        loadCKEditor: loadCKEditor,
+        unloadCKEditor: unloadCKEditor,
+        checkForUnsavedText: checkForUnsavedText
+    }
+})()
