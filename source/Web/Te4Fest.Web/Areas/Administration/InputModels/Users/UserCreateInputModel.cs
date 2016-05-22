afd270c63e01d1dee92c08a12797f00b9ce0bae1
KristianMariyanov <kristian.mariyanov@gmail.com>
2016-05-23 01:23:46 +0300

    Added models for users administration

---
+++ source/Web/Te4Fest.Web/Areas/Administration/InputModels/Users/UserCreateInputModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Te4Fest.Web.Areas.Administration.InputModels.Users
+{
+    using Te4Fest.Common.Mapping;
+    using Te4Fest.Data.Models;
+
+    public class UserCreateInputModel : IMapFrom<User>, IMapTo<User>
+    {
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public bool IsSamePassword { get; set; }
+    }
+}
---
+++ source/Web/Te4Fest.Web/Areas/Administration/InputModels/Users/UserEditInputModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Te4Fest.Web.Areas.Administration.InputModels.Users
+{
+    public class UserEditInputModel : UserCreateInputModel
+    {
+        public string Id { get; set; }
+    }
+}
