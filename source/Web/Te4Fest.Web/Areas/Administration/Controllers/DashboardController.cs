6e76aefa8ba310e1a6f344de818ccd3348b260d5
Teodor92 <teodor.ivanov92@gmail.com>
2016-05-24 00:05:16 +0300

    Added dashboard controller #15

---
+++ source/Web/Te4Fest.Web/Areas/Administration/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿namespace Te4Fest.Web.Areas.Administration.Controllers
+{
+    using System.Web.Mvc;
+
+    using Te4Fest.Web.Areas.Administration.Controllers.Base;
+
+    public class DashboardController : AdministrationController
+    {
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
