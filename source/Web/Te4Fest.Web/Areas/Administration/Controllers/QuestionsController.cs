8cee40e24b646b9ced038a3d22f90dba91970649
Teodor92 <teodor.ivanov92@gmail.com>
2016-05-24 00:02:43 +0300

    Added controller for new administrations

---
+++ source/Web/Te4Fest.Web/Areas/Administration/Controllers/QuestionsController.cs
@@ -0,0 +1,15 @@
+﻿namespace Te4Fest.Web.Areas.Administration.Controllers
+{
+    using System.Web.Mvc;
+
+    using Te4Fest.Web.Areas.Administration.Controllers.Base;
+
+    public class QuestionsController : AdministrationController
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
