27b4d856d0a703b71abd66f4601fd107a4bc2f7b
Teodor92 <teodor.ivanov92@gmail.com>
2016-05-24 02:09:22 +0300

    Added controller logic for all images actions #18

--- source/Web/Te4Fest.Web/Areas/Administration/Controllers/ImagesController.cs
+++ source/Web/Te4Fest.Web/Areas/Administration/Controllers/ImagesController.cs
@@ -1,33 +1,80 @@
 ﻿namespace Te4Fest.Web.Areas.Administration.Controllers
 {
+    using System.IO;
     using System.Linq;
     using System.Web.Mvc;
 
     using Te4Fest.Data.Models;
     using Te4Fest.Services.Administration.Contracts;
     using Te4Fest.Services.Common.Mapping.Contracts;
+    using Te4Fest.Web.Areas.Administration.Controllers.Base;
+    using Te4Fest.Web.Areas.Administration.InputModels.Images;
     using Te4Fest.Web.Areas.Administration.ViewModels.Images;
     using Te4Fest.Web.Common.Extensions;
 
-    using Web.Controllers.Base;
-
-    public class ImagesController : BaseController
+    public class ImagesController : MvcGridAdministrationController<Image, ImageGridViewModel, ImageCreateInputModel, ImageUpdateInputModel>
     {
-        private readonly IAdministrationService<Image> imageAdministrationService;
-        private readonly IMappingService mapingService;
-
         public ImagesController(
             IAdministrationService<Image> imageAdministrationService,
-            IMappingService mapingService)
+            IMappingService mapingService) : base(imageAdministrationService, mapingService)
         {
-            this.imageAdministrationService = imageAdministrationService;
-            this.mapingService = mapingService;
         }
 
-        public ActionResult Index()
+        public ActionResult Index() => this.View(this.GetData().OrderBy(x => x.Id));
+
+        [HttpGet]
+        public ActionResult Create()
         {
-            var imagesViewModels = this.mapingService.MapCollection<ImageGridViewModel>(this.imageAdministrationService.Read().OrderBy(x => x.Id));
-            return this.View(imagesViewModels);
+            return this.View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ImageCreateInputModel input)
+        {
+            var entity = this.BaseCreate(input);
+            if (entity != null)
+            {
+                this.TempData.AddSuccessMessage("Успешно запазихте изображение");
+                return this.RedirectToAction(nameof(this.Index));
+            }
+
+            return this.View(input);
+        }
+
+        [HttpGet]
+        public ActionResult Update()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Update(ImageUpdateInputModel input)
+        {
+            var entity = this.BaseUpdate(input, input.Id);
+            if (entity != null)
+            {
+                this.TempData.AddSuccessMessage("Успешно редактирахте модул");
+                return this.RedirectToAction(nameof(this.Index));
+            }
+
+            return this.View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int? id)
+        {
+            if (!id.HasValue)
+            {
+                this.TempData.AddDangerMessage("Няма такава снимка.");
+                return this.RedirectToAction<ImagesController>(x => x.Index());
+            }
+
+            this.BaseDestroy(id);
+
+            this.TempData.AddSuccessMessage("Успешно изтрихте изображение.");
+            return this.RedirectToAction<ImagesController>(x => x.Index());
         }
     }
 }
---
+++ source/Web/Te4Fest.Web/Areas/Administration/InputModels/Images/ImageUpdateInputModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Te4Fest.Web.Areas.Administration.InputModels.Images
+{
+    using System.Web.Mvc;
+
+    public class ImageUpdateInputModel : ImageCreateInputModel
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int Id { get; set; }
+    }
+}
