72f0dbadce3fa22adb5dbbeb34c93422f51db5eb
KristianMariyanov <kristian.mariyanov@gmail.com>
2016-05-27 20:46:54 +0300

    Added IUsersAdministrationService interface with create method

---
+++ source/Services/Te4Fest.Services.Administration/Contracts/IUsersInRolesAdministrationService.cs
@@ -0,0 +1,9 @@
+﻿namespace Te4Fest.Services.Administration.Contracts
+{
+    using Te4Fest.Data.Models;
+
+    public interface IUsersInRolesAdministrationService : IAdministrationService<UserInRole>
+    {
+        void Create(string userId, string roleId);
+    }
+}
