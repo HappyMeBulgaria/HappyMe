43eebe5c75d789113e663db6b0fe3d1ea85e0a9c
KristianMariyanov <kristian.mariyanov@gmail.com>
2016-05-27 20:47:48 +0300

    Added UsersAdministrationService with method for getting user by username

---
+++ source/Services/Te4Fest.Services.Administration/Contracts/IUsersAdministrationService.cs
@@ -0,0 +1,9 @@
+﻿namespace Te4Fest.Services.Administration.Contracts
+{
+    using Te4Fest.Data.Models;
+
+    public interface IUsersAdministrationService : IAdministrationService<User>
+    {
+        User GetByUsername(string username);
+    }
+}
