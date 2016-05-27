c48ad85ac0058c2841cd53310760caec7cb7ba13
KristianMariyanov <kristian.mariyanov@gmail.com>
2016-05-27 20:53:43 +0300

    Added EnabledAttribute for setting disabled attribute of the inputs

---
+++ source/Web/Te4Fest.Web.Common/Attributes/EnabledAttribute.cs
@@ -0,0 +1,25 @@
+﻿namespace Te4Fest.Web.Common.Attributes
+{
+    using System;
+    using System.Web.Mvc;
+
+    public class EnabledAttribute : Attribute, IMetadataAware
+    {
+        private readonly bool enabled;
+
+        public EnabledAttribute(bool enabled)
+        {
+            this.enabled = enabled;
+        }
+
+        void IMetadataAware.OnMetadataCreated(ModelMetadata metadata)
+        {
+            this.OnMetadataCreated(metadata);
+        }
+
+        public void OnMetadataCreated(ModelMetadata metadata)
+        {
+            metadata.AdditionalValues["enabled"] = this.enabled;
+        }
+    }
+}
