8db9968a192c0a62bfe461224c700a0e16cfa82f
KristianMariyanov <kristian.mariyanov@gmail.com>
2016-05-27 20:45:31 +0300

    Override IdentityUserRole to be able to add IEntity interface

---
+++ source/Data/Te4Fest.Data.Models/UserInRole.cs
@@ -0,0 +1,10 @@
+﻿namespace Te4Fest.Data.Models
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    using Te4Fest.Data.Contracts;
+
+    public class UserInRole : IdentityUserRole, IEntity
+    {
+    }
+}
