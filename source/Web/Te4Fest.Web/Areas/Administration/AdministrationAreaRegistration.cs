f2bbcf286d4b103072c904ca03b160b242eccbe9
KristianMariyanov <kristian.mariyanov@gmail.com>
2016-05-27 20:52:34 +0300

    Configure username to be able to use as a parameter in actions

--- source/Web/Te4Fest.Web/Areas/Administration/AdministrationAreaRegistration.cs
+++ source/Web/Te4Fest.Web/Areas/Administration/AdministrationAreaRegistration.cs
@@ -9,6 +9,10 @@
         public override void RegisterArea(AreaRegistrationContext context) 
         {
             context.MapRoute(
+                "Administration_with_username",
+                "Administration/{controller}/{action}/{username}");
+
+            context.MapRoute(
                 "Administration_default",
                 "Administration/{controller}/{action}/{id}",
                 new { action = "Index", id = UrlParameter.Optional });
