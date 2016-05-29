61a97968a3bee63d615fd9743930f97a45c597dd
KristianMariyanov <kristian.mariyanov@gmail.com>
2016-05-29 16:43:35 +0300

    Added Administration for UsersAsnwers

---
+++ source/Web/HappyMe.Web/Areas/Administration/Controllers/UsersAnswersController.cs
@@ -0,0 +1,54 @@
+﻿namespace HappyMe.Web.Areas.Administration.Controllers
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using HappyMe.Data.Models;
+    using HappyMe.Services.Administration.Contracts;
+    using HappyMe.Services.Common.Mapping.Contracts;
+    using HappyMe.Services.Data.Contracts;
+    using HappyMe.Web.Areas.Administration.Controllers.Base;
+    using HappyMe.Web.Areas.Administration.ViewModels.UsersAnswers;
+    using HappyMe.Web.Common.Extensions;
+
+    public class UsersAnswersController : MvcGridAdministrationReadAndDeleteController<UserAnswer, UserAnswerGridViewModel>
+    {
+        public UsersAnswersController(
+            IUsersDataService userData,
+            IUsersAnswersAdministrationService dataRepository, 
+            IMappingService mappingService) 
+            : base(userData, dataRepository, mappingService)
+        {
+        }
+
+        public ActionResult Index()
+        {
+            IEnumerable<UserAnswerGridViewModel> usersAnswers;
+            if (this.User.IsAdmin())
+            {
+                usersAnswers = this.GetData().OrderBy(x => x.CreatedOn);
+            }
+            else
+            {
+                usersAnswers = this.MappingService
+                    .MapCollection<UserAnswerGridViewModel>(
+                        (this.AdministrationService as IUsersAnswersAdministrationService)
+                            .GetChildrenAnswers(this.UserProfile.Id))
+                    .OrderBy(m => m.CreatedOn);
+            }
+
+            return this.View(usersAnswers);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(string id, int secondId)
+        {
+            this.BaseDestroy(id, secondId);
+
+            this.TempData.AddSuccessMessage("Успешно изтрихте потребителски отговор");
+            return this.RedirectToAction(nameof(this.Index));
+        }
+    }
+}
---
+++ source/Web/HappyMe.Web/Areas/Administration/ViewModels/UsersAnswers/UserAnswerGridViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace HappyMe.Web.Areas.Administration.ViewModels.UsersAnswers
+{
+    using System;
+
+    using AutoMapper;
+
+    using HappyMe.Common.Mapping;
+    using HappyMe.Data.Models;
+
+    public class UserAnswerGridViewModel : IMapFrom<UserAnswer>, IHaveCustomMappings
+    {
+        public string UserName { get; set; }
+
+        public string AnswerText { get; set; }
+
+        public string UserId { get; set; }
+
+        public int AnswerId { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public void CreateMappings(IMapperConfiguration configuration)
+        {
+            configuration.CreateMap<UserAnswer, UserAnswerGridViewModel>()
+                .ForMember(m => m.UserName, opt => opt.MapFrom(e => e.User.UserName));
+        }
+    }
+}
