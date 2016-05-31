0c0ed0fea6751782632bd83303fd00e8bdd0cc83
mhristoforova <mhristoforova@gmail.com>
2016-06-01 01:18:48 +0300

    Fixed nav, fixed path to jQuery

--- source/Web/HappyMe.Web/Scripts/custom/index-script.js
+++ source/Web/HappyMe.Web/Scripts/custom/index-script.js
@@ -1,20 +1,26 @@
-function hideElement(){
-	document.getElementById("button-menu").style.display="none";
+function hideElement() {
+    document.getElementById("button-menu").style.display = "none";
 }
 
-function showElement(){
-	document.getElementById("nav-hidden").style.display="inline-block";
+function showElement() {
+    document.getElementById("nav-hidden").style.display = "inline-block";
 }
 
-$(function(){
- $(".scroll").click(function(event){
-         event.preventDefault();
-         var dest=0;
-         if($(this.hash).offset().top > $(document).height()-$(window).height()){
-              dest=$(document).height()-$(window).height();
-         }else{
-              dest=$(this.hash).offset().top;
-         }
-         $('html,body').animate({scrollTop:dest}, 1000,'swing');
-     });
- });
+$(function () {
+    $(".scroll").click(function (event) {
+        event.preventDefault();
+        var dest = 0;
+        if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+            dest = $(document).height() - $(window).height();
+        } else {
+            dest = $(this.hash).offset().top;
+        }
+        $('html,body').animate({
+            scrollTop: dest
+        }, 1000, 'swing');
+    });
+});
+
+$("play-game").hover(function () {
+    $(this).toggleClass("hvr-grow");
+});
