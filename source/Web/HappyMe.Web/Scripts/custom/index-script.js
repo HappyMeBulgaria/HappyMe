95c6643dd0e3af2342364f4070ecb4a427f2a0b1
mhristoforova <mhristoforova@gmail.com>
2016-05-31 23:12:19 +0300

    Started styling the navigation, added BloggerSans Font to project

---
+++ source/Web/HappyMe.Web/Scripts/custom/index-script.js
@@ -0,0 +1,7 @@
+function hideElement(){
+	document.getElementById("button-menu").style.display="none";
+}
+
+function showElement(){
+	document.getElementById("nav-hidden").style.display="inline-block";
+}
