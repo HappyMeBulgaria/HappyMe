6fbc21503a5422a855587e9481ef9b97477e86e2
mhristoforova <mhristoforova@gmail.com>
2016-06-01 00:15:46 +0300

    Improved navigation styles

--- source/Web/HappyMe.Web/Scripts/custom/index-script.js
+++ source/Web/HappyMe.Web/Scripts/custom/index-script.js
@@ -5,3 +5,18 @@ function hideElement(){
 function showElement(){
 	document.getElementById("nav-hidden").style.display="inline-block";
 }
+
+$(function(){
+ $(".scroll").click(function(event){
+         event.preventDefault();
+         //calculate destination place
+         var dest=0;
+         if($(this.hash).offset().top > $(document).height()-$(window).height()){
+              dest=$(document).height()-$(window).height();
+         }else{
+              dest=$(this.hash).offset().top;
+         }
+         //go to destination
+         $('html,body').animate({scrollTop:dest}, 1000,'swing');
+     });
+ });
