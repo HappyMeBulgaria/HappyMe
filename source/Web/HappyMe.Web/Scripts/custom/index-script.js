2abe485dd7818a8491edb29bb47f40da28f1c47e
mhristoforova <mhristoforova@gmail.com>
2016-06-01 00:58:20 +0300

    Added landing page styling

--- source/Web/HappyMe.Web/Scripts/custom/index-script.js
+++ source/Web/HappyMe.Web/Scripts/custom/index-script.js
@@ -9,14 +9,12 @@ function showElement(){
 $(function(){
  $(".scroll").click(function(event){
          event.preventDefault();
-         //calculate destination place
          var dest=0;
          if($(this.hash).offset().top > $(document).height()-$(window).height()){
               dest=$(document).height()-$(window).height();
          }else{
               dest=$(this.hash).offset().top;
          }
-         //go to destination
          $('html,body').animate({scrollTop:dest}, 1000,'swing');
      });
  });
