3a3837bcfd638a7ad9aaef832dcbba0d25c68430
mhristoforova <mhristoforova@gmail.com>
2016-06-01 03:19:40 +0300

    Imported and Implemented SlickSlider

--- source/Web/HappyMe.Web/Scripts/custom/index-script.js
+++ source/Web/HappyMe.Web/Scripts/custom/index-script.js
@@ -22,3 +22,17 @@ $(function () {
 });
 
 
+$(".slider").slick({
+    autoplay: true,
+    dots: true,
+    responsive: [{ 
+        breakpoint: 500,
+        settings: {
+            dots: false,
+            arrows: false,
+            infinite: false,
+            slidesToShow: 2,
+            slidesToScroll: 2
+        } 
+    }]
+});
