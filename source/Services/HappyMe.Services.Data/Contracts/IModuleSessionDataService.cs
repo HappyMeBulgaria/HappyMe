91e9da801376d03a833e21a1c4c663b99887bca0
Teodor92 <teodor.ivanov92@gmail.com>
2016-06-01 21:24:17 +0300

    Started work on question supply logic.

---
+++ source/Services/HappyMe.Services.Data/Contracts/IModuleSessionDataService.cs
@@ -0,0 +1,9 @@
+﻿namespace HappyMe.Services.Data.Contracts
+{
+    using HappyMe.Data.Models;
+
+    public interface IModuleSessionDataService
+    {
+        Question NextQuestion(int moduleSessionId, string userId);
+    }
+}
---
+++ source/Services/HappyMe.Services.Data/ModuleSessionDataService.cs
@@ -0,0 +1,39 @@
+﻿namespace HappyMe.Services.Data
+{
+    using System.Linq;
+
+    using HappyMe.Data.Contracts.Repositories;
+    using HappyMe.Data.Models;
+    using HappyMe.Services.Data.Contracts;
+
+    using MoreDotNet.Extentions.Common;
+
+    public class ModuleSessionDataService : IModuleSessionDataService
+    {
+        private readonly IRepository<ModuleSession> moduleSessionsRepository;
+        private readonly IRepository<UserAnswer> userAnswersRepository;
+
+        public ModuleSessionDataService(
+            IRepository<ModuleSession> moduleSessionsRepository,
+            IRepository<UserAnswer> userAnswersRepository)
+        {
+            this.moduleSessionsRepository = moduleSessionsRepository;
+            this.userAnswersRepository = userAnswersRepository;
+        }
+
+        public Question NextQuestion(int moduleSessionId, string userId)
+        {
+            var answerdQuestioIds =
+                this.userAnswersRepository.All()
+                    .Where(x => x.ModuleInstanceId == moduleSessionId && x.UserId == userId && x.Answer.IsCorrect)
+                    .Select(x => x.Answer.QuestionId);
+
+            var unanswerdQuestions =
+                this.moduleSessionsRepository
+                    .GetById(moduleSessionId)
+                    .Module.Questions.Where(x => !answerdQuestioIds.Contains(x.Id));
+
+            return unanswerdQuestions.FirstOrDefault();
+        }
+    }
+}
