4b37af64511a575a8dcb433c5bdb57fa845aae8e
Teodor92 <teodor.ivanov92@gmail.com>
2016-06-04 21:51:13 +0300

    Added statistics services

---
+++ source/Services/HappyMe.Services.Administration/Contracts/IChildrenStatisticsService.cs
@@ -0,0 +1,17 @@
+﻿namespace HappyMe.Services.Administration.Contracts
+{
+    using System.Linq;
+
+    using HappyMe.Services.Administration.Models;
+
+    public interface IChildrenStatisticsService
+    {
+        IQueryable<ChildAnswerRatoStatistic> GetWrongRightAnswersStatistics(string childId);
+
+        IQueryable<ModuleSessionStatistic> GetModuleSessionStatistics(string childId);
+
+        IQueryable<ModulePlayedTimesStatistic> GetModulePlayedTimesStatistics(string childId);
+
+        IQueryable<ModulePlayedTimesStatisticFull> GetModulePlayedTimesStatisticsForParentsChildren(string parentId);
+    }
+}
