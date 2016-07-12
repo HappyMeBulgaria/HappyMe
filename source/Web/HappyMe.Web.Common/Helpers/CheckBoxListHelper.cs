d0a966c48961e5a1028c45dc3d7834f89fba5519
Teodor92 <teodor.ivanov92@gmail.com>
2016-07-13 00:52:49 +0300

    Added checkbox list helpers.

---
+++ source/Web/HappyMe.Web.Common/Helpers/CheckBoxListHelper.cs
@@ -0,0 +1,81 @@
+﻿namespace HappyMe.Web.Common.Helpers
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
+    using MoreDotNet.Extentions.Common;
+
+    public static class HtmlHelperExtensions
+    {
+        /// <summary>
+        /// Returns a checkbox for each of the provided <paramref name="items"/>.
+        /// </summary>
+        public static MvcHtmlString CheckBoxListFor<TModel, TValue>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TValue>> expression, IEnumerable<SelectListItem> items, object htmlAttributes = null)
+        {
+            var listName = ExpressionHelper.GetExpressionText(expression);
+            var metaData = ModelMetadata.FromLambdaExpression(expression, htmlHelper.ViewData);
+
+            items = GetCheckboxListWithDefaultValues(metaData.Model, items);
+            return htmlHelper.CheckBoxList(listName, items, htmlAttributes);
+        }
+
+        /// <summary>
+        /// Returns a checkbox for each of the provided <paramref name="items"/>.
+        /// </summary>
+        public static MvcHtmlString CheckBoxList(this HtmlHelper htmlHelper, string listName, IEnumerable<SelectListItem> items, object htmlAttributes = null)
+        {
+            var container = new TagBuilder("div");
+            foreach (var item in items)
+            {
+                var label = new TagBuilder("label");
+                label.MergeAttribute("class", "checkbox"); // default class
+                label.MergeAttributes(new RouteValueDictionary(htmlAttributes), true);
+
+                var cb = new TagBuilder("input");
+                cb.MergeAttribute("type", "checkbox");
+                cb.MergeAttribute("name", listName);
+                cb.MergeAttribute("value", item.Value ?? item.Text);
+                if (item.Selected)
+                {
+                    cb.MergeAttribute("checked", "checked");
+                }
+
+                label.InnerHtml = cb.ToString(TagRenderMode.SelfClosing) + item.Text;
+
+                container.InnerHtml += label.ToString();
+            }
+
+            return new MvcHtmlString(container.ToString());
+        }
+
+        private static IEnumerable<SelectListItem> GetCheckboxListWithDefaultValues(object defaultValues, IEnumerable<SelectListItem> selectList)
+        {
+            var defaultValuesList = defaultValues as IEnumerable;
+
+            if (defaultValuesList == null)
+            {
+                return selectList;
+            }
+
+            var values =
+                defaultValuesList.Cast<object>().Select(value => Convert.ToString(value, CultureInfo.CurrentCulture));
+
+            var selectedValues = new HashSet<string>(values, StringComparer.OrdinalIgnoreCase);
+            var newSelectList = new List<SelectListItem>();
+
+            selectList.ForEach(item =>
+            {
+                item.Selected = (item.Value != null) ? selectedValues.Contains(item.Value) : selectedValues.Contains(item.Text);
+                newSelectList.Add(item);
+            });
+
+            return newSelectList;
+        }
+    }
+}
