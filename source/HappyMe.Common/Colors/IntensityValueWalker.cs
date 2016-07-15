4f1f5d854e48b65b2f80881473b66d6f48cabcb0
Teodor92 <teodor.ivanov92@gmail.com>
2016-07-15 22:30:42 +0300

    Added color generation logic

---
+++ source/HappyMe.Common/Colors/IntensityValueWalker.cs
@@ -0,0 +1,49 @@
+namespace HappyMe.Common.Colors
+{
+    public class IntensityValueWalker
+    {
+        public IntensityValueWalker()
+        {
+            this.Current = new IntensityValue(null, 1 << 7, 1);
+        }
+
+        public IntensityValue Current { get; set; }
+
+        public void MoveNext()
+        {
+            if (this.Current.Parent == null)
+            {
+                this.Current = this.Current.ChildA;
+            }
+            else if (this.Current.Parent.ChildA == this.Current)
+            {
+                this.Current = this.Current.Parent.ChildB;
+            }
+            else
+            {
+                var levelsUp = 1;
+                this.Current = this.Current.Parent;
+                while (this.Current.Parent != null && this.Current == this.Current.Parent.ChildB)
+                {
+                    this.Current = this.Current.Parent;
+                    levelsUp++;
+                }
+
+                if (this.Current.Parent != null)
+                {
+                    this.Current = this.Current.Parent.ChildB;
+                }
+                else
+                {
+                    levelsUp++;
+                }
+
+                for (var i = 0; i < levelsUp; i++)
+                {
+                    this.Current = this.Current.ChildA;
+                }
+
+            }
+        }
+    }
+}
