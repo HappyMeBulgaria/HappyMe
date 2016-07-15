8cc3c852a07c096ae72756ea72af30c78cd42eef
Teodor92 <teodor.ivanov92@gmail.com>
2016-07-15 20:17:31 +0300

    Started implementing random answer selection logic.

--- source/Services/HappyMe.Services.Data/AnswersDataService.cs
+++ source/Services/HappyMe.Services.Data/AnswersDataService.cs
@@ -1,18 +1,27 @@
 ﻿namespace HappyMe.Services.Data
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
     using System.Threading.Tasks;
 
+    using HappyMe.Common.Models;
     using HappyMe.Data.Contracts.Repositories;
     using HappyMe.Data.Models;
     using HappyMe.Services.Data.Contracts;
 
     public class AnswersDataService : IAnswersDataService
     {
-        private readonly IRepository<UserAnswer> userAnswersRepository; 
+        private readonly IRepository<UserAnswer> userAnswersRepository;
+        private readonly IRepository<Answer> answersRepository;
 
-        public AnswersDataService(IRepository<UserAnswer> userAnswersRepository)
+        public AnswersDataService(
+            IRepository<UserAnswer> userAnswersRepository,
+            IRepository<Answer> answersRepository)
         {
             this.userAnswersRepository = userAnswersRepository;
+            this.answersRepository = answersRepository;
         }
 
         public async Task AnswerAsUser(string userId, int answerId, int moduleSessionId)
@@ -27,6 +36,33 @@
             var newAnswer = new UserAnswer(answerId, moduleSessionId);
             this.userAnswersRepository.Add(newAnswer);
             await this.userAnswersRepository.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Answer>> GetRandomAnswersForQuestion(Question question, int answersCount)
+        {
+            var correctAnswersForQuestion = question.Answers
+                .Where(x => x.IsCorrect)
+                .ToList();
+
+            if (correctAnswersForQuestion == null || !correctAnswersForQuestion.Any())
+            {
+                throw new ArgumentException("Question must have atleast one correct answer!", nameof(question));
+            }
+
+            if (question.Type == QuestionType.ColorQuestion)
+            {
+                return null;
+            }
+
+            var randomAnswers =
+                this.answersRepository.All()
+                    .Where(x => x.Question.Type == question.Type 
+                    && correctAnswersForQuestion.All(y => x.Id != y.Id))
+                    .OrderBy(x => new Guid())
+                    .Take(answersCount)
+                    .ToListAsync();
+
+            return await randomAnswers;
         }
     }
 }
--- source/Services/HappyMe.Services.Data/Contracts/IAnswersDataService.cs
+++ source/Services/HappyMe.Services.Data/Contracts/IAnswersDataService.cs
@@ -2,6 +2,8 @@
 {
     using System.Threading.Tasks;
 
+    using HappyMe.Common.Models;
+    using HappyMe.Data.Models;
     using HappyMe.Services.Common;
 
     public interface IAnswersDataService : IService
@@ -9,5 +11,7 @@
         Task AnswerAsUser(string userId, int answerId, int moduleSessionId);
 
         Task AnswerAsAnonymous(int answerId, int moduleSessionId);
+
+        Task<Answer> GetRandomAnswersForQuestionType(QuestionType questionType);
     }
 }
