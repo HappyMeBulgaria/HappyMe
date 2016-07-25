8df4dd3adbe18d789834dc0b6da594ec65d848b2
Teodor92 <teodor.ivanov92@gmail.com>
2016-07-25 18:54:21 +0300

    More web tests.

---
+++ source/Tests/HappyMe.Tests.Web/ModulesControllerTests.cs
@@ -0,0 +1,33 @@
+﻿namespace HappyMe.Tests.Web
+{
+    using HappyMe.Services.Common.Mapping;
+    using HappyMe.Services.Common.Mapping.Contracts;
+    using HappyMe.Services.Data;
+    using HappyMe.Services.Data.Contracts;
+    using HappyMe.Tests.Web.Common;
+    using HappyMe.Web.Controllers;
+
+    using Moq;
+
+    public class ModulesControllerTests : BaseWebTests
+    {
+        private readonly ModulesController modulesController;
+        private readonly IModulesDataService modulesDataService;
+        private readonly IModuleSessionDataService moduleSessionDataService;
+        private readonly IMappingService mappingService;
+
+        public ModulesControllerTests()
+        {
+            this.modulesDataService = new Mock<IModulesDataService>().Object;
+
+            this.moduleSessionDataService = new Mock<IModuleSessionDataService>().Object;
+
+            this.mappingService = new AutoMapperMappingService();
+
+            this.modulesController = new ModulesController(
+                this.modulesDataService,
+                this.moduleSessionDataService,
+                this.mappingService);
+        }
+    }
+}
