00b4d790f3ed692114ecdd4d6641ce9626a6e1b3
Teodor92 <teodor.ivanov92@gmail.com>
2017-04-03 21:38:19 +0200

    Revert "Moved all reaming .resx files to separated project. Note: need to fix the code generation, because VS reseted all designer properties... fml."
    This reverts commit 10d21240e429327844830f0b66be21b2741f7bc1.

--- source/Web/HappyMe.Web.Models/Areas/Administration/ViewModels/ChildrenStatistics/ChildStatisticsIndexViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HappyMe.Web.Models.Areas.Administration.ViewModels.ChildrenStatistics
-{
-    public class ChildStatisticsIndexViewModel
-    {
-        public string ChildId { get; set; }
-
-        public string ChildUserName { get; set; }
-    }
-}
--- source/Web/HappyMe.Web.Models/Areas/Administration/ViewModels/Dashboard/DashboardIndexViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HappyMe.Web.Models.Areas.Administration.ViewModels.Dashboard
-{
-    using System.Collections.Generic;
-
-    public class DashboardIndexViewModel
-    {
-        public IEnumerable<ChildViewModel> CurrentUserChildren { get; set; }
-    }
-}
