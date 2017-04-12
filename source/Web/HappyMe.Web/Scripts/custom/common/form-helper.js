bf803a9e3f62b2c8259061fa3c21b6054a0a6f7f
KristianMariyanov <kristian.mariyanov@gmail.com>
2017-04-12 23:42:23 +0300

    Fix erros in form-helper.js

--- source/Web/HappyMe.Web/Scripts/custom/common/form-helper.js
+++ source/Web/HappyMe.Web/Scripts/custom/common/form-helper.js
@@ -3,12 +3,12 @@
 HappyMe.FormHelper = (function () {
     'use strict';
 
-    function confirmSubmit(formSelector, message) {
-       $(selector).on('submit',
-            function () {
-                return confirm(message);
-            });
-   }
+    function confirmSubmit (formSelector, message) {
+        $(selector).on('submit',
+             function () {
+                 return confirm(message);
+             });
+    }
 
     return {
         confirmSubmit: confirmSubmit
